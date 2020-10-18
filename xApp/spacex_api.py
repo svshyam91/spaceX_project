9ebb422cf78e07420f19e7d2b3d3afa3acecb220
svshyam91 <svshyam111@gmail.com>
2020-10-19 04:08:20 +0530

    Final Submit

--- xApp/spacex_api.py
+++ xApp/spacex_api.py
@@ -1,7 +1,16 @@
 import requests
 
-def spaceX_api():
-    apiURL = "https://api.spacexdata.com/v3/launches?limit=20"
+def spaceX_api(all_filters):
+    if(all_filters == None):
+        apiURL = "https://api.spacexdata.com/v3/launches?limit=50"
+    else:
+        filter_string = ''
+        for key in all_filters:
+            filter_string += '&'+key+'='+all_filters[key]
+        # filter_string = filter_string[:-1]
+
+        apiURL = "https://api.spacexdata.com/v3/launches?limit=50"+filter_string
+        print("filter String",filter_string)
     res = requests.get(apiURL)
 
     if res.status_code == 200: 
@@ -15,14 +24,16 @@ def spaceX_api():
                 'mission_ids': data['mission_id'],
                 'launch_year': data['launch_year'],
                 'launch_success': data['launch_success'],
-                'image': data['links']['mission_patch_small']
+                'image': data['links']['mission_patch_small'],
+                'land_success': data['rocket']['first_stage']['cores'][0]['land_success']
                 # 'land_success': data['rocket']['first_stage']['cores']
             }
             all_data.append(spacex_data)
+        # print(all_data)
         return all_data
     else:
         return 404
 
 
-if __name__ == '__main__':
-    spaceX_api()
+# if __name__ == '__main__':
+#     spaceX_api()
--- xApp/views.py
+++ xApp/views.py
@@ -3,6 +3,21 @@ from django.http import HttpResponse
 from . import spacex_api
 
 def index(request):
-    all_data = spacex_api.spaceX_api()
+    all_data = spacex_api.spaceX_api(None)
     return render(request, 'xApp/index.html', {'all_data': all_data})
+
+def filters(request):
+    all_filters = {}
+    if request.method == 'GET':
+        print('something')
+        if 'launch_success' in request.GET:
+            all_filters['launch_success'] = request.GET['launch_success']
+        if 'land_success' in request.GET:
+            all_filters['land_success'] = request.GET['land_success']
+        if 'launch_year' in request.GET:
+            all_filters['launch_year'] = request.GET['launch_year']
+
+        all_data = spacex_api.spaceX_api(all_filters)    
+
+    return render(request, 'xApp/filters.html', {'all_data': all_data})
 
